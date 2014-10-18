2f3026228279bb1ee2a5de464a080e9bb07a4e0b
Alban Browaeys <prahal@yahoo.com>
2014-10-19 01:55:09 +0200

    Restore device_add/remove callbacks as bus_set_iommu needs them
    Revert "iommu: exynos: remove useless device_add/remove callbacks"
    
    If those are missing bus_set_iommu return -ENODEV in add_iommu_group.
    
    This reverts commit c99b16b3b056dfc18999f7deee6c89370826da63.
    
    Conflicts:
    	drivers/iommu/exynos-iommu.c

--- drivers/iommu/exynos-iommu.c
+++ drivers/iommu/exynos-iommu.c
@@ -1105,6 +1105,32 @@ static int exynos_iommu_of_xlate(struct device *dev,
 	return 1;
 }
 
+static int exynos_iommu_add_device(struct device *dev)
+{
+	struct iommu_group *group;
+	int ret;
+
+	group = iommu_group_get(dev);
+
+	if (!group) {
+		group = iommu_group_alloc();
+		if (IS_ERR(group)) {
+			dev_err(dev, "Failed to allocate IOMMU group\n");
+			return PTR_ERR(group);
+		}
+	}
+
+	ret = iommu_group_add_device(group, dev);
+	iommu_group_put(group);
+
+	return ret;
+}
+
+static void exynos_iommu_remove_device(struct device *dev)
+{
+	iommu_group_remove_device(dev);
+}
+
 static const struct iommu_ops exynos_iommu_ops = {
 	.domain_init = exynos_iommu_domain_init,
 	.domain_destroy = exynos_iommu_domain_destroy,
@@ -1113,6 +1139,8 @@ static const struct iommu_ops exynos_iommu_ops = {
 	.map = exynos_iommu_map,
 	.unmap = exynos_iommu_unmap,
 	.iova_to_phys = exynos_iommu_iova_to_phys,
+	.add_device = exynos_iommu_add_device,
+	.remove_device = exynos_iommu_remove_device,
 	.pgsize_bitmap = SECT_SIZE | LPAGE_SIZE | SPAGE_SIZE,
 	.of_xlate = exynos_iommu_of_xlate,
 };
